ccb11e27195b28cb2a6e7b83023d51b242ed74ce
zhumagulovt <zhumagulovtorokeldi@gmail.com>
2023-05-07 20:25:15 +0600

    test: add tests for reset password

--- tests/users/test_password.py
+++ tests/users/test_password.py
@@ -1,3 +1,6 @@
+from unittest.mock import patch
+from urllib.parse import parse_qs, urlparse
+
 import pytest
 from django.urls import reverse
 from rest_framework import status
@@ -31,3 +34,35 @@ class TestPassword:
         if response.status_code == status.HTTP_200_OK:
             user.refresh_from_db()
             assert user.check_password(new_password) is True
+
+    @patch("project_manager.users.tasks.send_reset_password_link_task.delay")
+    def test_reset_password(
+        self, mock_reset_password, api_client, user_factory
+    ):
+        user = user_factory()
+
+        response = api_client.post(
+            reverse("reset_password"), data={"email": user.email}
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+
+        # parse link in email message to get uid and token
+        args, kwargs = mock_reset_password.call_args
+        query_params = parse_qs(urlparse(kwargs["reset_password_link"]).query)
+
+        data = {
+            "uid": query_params["uid"][0],
+            "token": query_params["token"][0],
+            "password": "NewPAssword_",
+            "password_confirm": "NewPAssword_",
+        }
+
+        response = api_client.post(
+            reverse("reset_password_complete"), data=data
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+
+        user.refresh_from_db()
+        assert user.check_password(data["password"]) is True
