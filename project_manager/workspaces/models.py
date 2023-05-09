99d2f0bdc543d7a5c91a1327e84b8ba21ef188f4
zhumagulovt <zhumagulovtorokeldi@gmail.com>
2023-05-10 00:26:09 +0600

    feat: add Workspace, UserWorkspace, List models

--- project_manager/workspaces/models.py
+++ project_manager/workspaces/models.py
@@ -1,1 +1,40 @@
-# Create your models here.
+from django.db import models
+
+from project_manager.users.models import User
+
+
+class ContentModel(models.Model):
+    """Abstract model for repeating fields: title, content"""
+
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+
+    class Meta:
+        abstract = True
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Workspace(ContentModel):
+    owner = models.ForeignKey(
+        User, related_name="own_workspaces", on_delete=models.CASCADE
+    )
+    avatar = models.ImageField(upload_to="workspaces/avatars", blank=True)
+
+
+class UserWorkspace(models.Model):
+    user = models.ForeignKey(
+        User, related_name="workspaces", on_delete=models.CASCADE
+    )
+    workspace = models.ForeignKey(
+        Workspace, related_name="users", on_delete=models.CASCADE
+    )
+    is_admin = models.BooleanField(default=False)
+
+
+class List(ContentModel):
+    workspace = models.ForeignKey(
+        Workspace, related_name="lists", on_delete=models.CASCADE
+    )
+    avatar = models.ImageField(upload_to="lists/avatars", blank=True)
