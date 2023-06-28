e82334d38698572262eb3f6ea2cc270ab2a21bce
zhumagulovt <zhumagulovtorokeldi@gmail.com>
2023-06-28 23:11:09 +0600

    fix: m2m field between Workspace and User

--- project_manager/workspaces/models.py
+++ project_manager/workspaces/models.py
@@ -21,19 +21,18 @@ class Workspace(ContentModel):
         User, related_name="own_workspaces", on_delete=models.CASCADE
     )
     avatar = models.ImageField(upload_to="workspaces/avatars", blank=True)
+    users = models.ManyToManyField(
+        User, related_name="users", through="UserWorkspace"
+    )
 
 
 class UserWorkspace(models.Model):
-    user = models.ForeignKey(
-        User, related_name="workspaces", on_delete=models.CASCADE
-    )
-    workspace = models.ForeignKey(
-        Workspace, related_name="users", on_delete=models.CASCADE
-    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE)
     is_admin = models.BooleanField(default=False)
 
 
 class TaskList(ContentModel):
     workspace = models.ForeignKey(
-        Workspace, related_name="lists", on_delete=models.CASCADE
+        Workspace, related_name="task_lists", on_delete=models.CASCADE
     )
